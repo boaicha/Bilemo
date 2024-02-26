6e17206f11589bc3acd54254fbc1e2c3bde3d28f
boaicha <aichabouhouch.ab@gmail.com>
2024-02-26 15:30:45 +0100

    Crud produit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+        $products = $doctrine
+            ->getRepository(Product::class)
+            ->findAll();
+
+        $data = [];
+
+        foreach ($products as $product) {
+            $data[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'description' => $product->getDescription(),
+                'price' => $product->getPrice(),
+
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    #[Route('/product/{id}', name: 'product_show', methods:['get'] )]
+    public function show(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $product = $doctrine->getRepository(product::class)->find($id);
+
+        if (!$product) {
+
+            return $this->json('No product found for id ' . $id, 404);
+        }
+
+        $data =  [
+            'id' => $product->getId(),
+            'name' => $product->getName(),
+            'description' => $product->getDescription(),
+            'price' => $product->getPrice(),
+        ];
+
+        return $this->json($data);
+    }
+
+        #[Route('/products/{id}', name: 'product_update', methods:['put', 'patch'] )]
+    public function update(ManagerRegistry $doctrine, Request $request, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(product::class)->find($id);
+
+        if (!$product) {
+            return $this->json('No product found for id' . $id, 404);
+        }
+
+        $product->setName($request->request->get('name'));
+        $product->setDescription($request->request->get('description'));
+        $product->setPrice($request->request->get('Price'));
+        $entityManager->flush();
+
+        $data =  [
+            'id' => $product->getId(),
+            'name' => $product->getName(),
+            'description' => $product->getDescription(),
+            'price' => $product->getPrice(),
+        ];
+
+        return $this->json($data);
+    }
+    #[Route('/products/{id}', name: 'product_delete', methods:['delete'] )]
+    public function delete(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            return $this->json('Aucun produit trouvé pour cet identifiant' . $id, 404);
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->json('Suppression réussie .' . $id);
+    }
+
+}
+

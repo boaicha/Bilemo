1c5c0670460753117f74b060a93f8f9cd9059bdd
boaicha <aichabouhouch.ab@gmail.com>
2024-02-26 15:41:22 +0100

    Crud user

---
+++ src/Entity/User.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+
+    #[ORM\Column]
+    private ?string $name;
+
+    #[ORM\Column]
+    private ?string $email;
+
+    #[ORM\Column]
+    private ?string $password;
+
+
+//    /**
+//     * @ORM\ManyToOne(targetEntity="App\Entity\Customer", inversedBy="users")
+//     * @ORM\JoinColumn(nullable=false)
+//     */
+//    private $customer;
+
+    // #[ORM\Column]
+    // private array $roles = [];
+
+    #[ORM\ManyToOne(targetEntity:Customer::class, inversedBy:"users")]
+    #[ORM\JoinColumn(nullable:true)]
+    private Customer $customer;
+
+
+    /**
+     * @return string|null
+     */
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    /**
+     * @param string|null $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
+
+    /**
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getCustomer(): Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(Customer $customer): void
+    {
+        $this->customer = $customer;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+
+
+
+
+}
